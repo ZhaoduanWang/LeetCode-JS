f835b69392989e6c6582c19c4c82bd863c12ba76
Danny Wong <DannyWang0619@gmail.com>
2020-03-26 11:56:16 -0600

    Ltc0198

---
+++ LtcE0198HouseRobber.js
@@ -0,0 +1,20 @@
+var rob = function(nums){
+    var dp = Array(nums.length);
+    for (let i=0; i<nums.length; i++)
+        dp[i] = 0;
+    dp[0] = nums[0];
+    
+    for (let i=1; i<nums.length; i++)
+        for (let j=0; j<i; j++)
+            if (i>j+1) {
+                dp[i] = Math.max( (dp[j]+nums[i]), dp[i] );
+            } else { 
+                // i = j+1
+                dp[i] = Math.max( dp[i], dp[j], nums[i]);
+            }
+    
+    console.log(dp);
+}
+
+rob([1,2,3,1]);
+rob([2,7,9,3,1]);
