d1e378499fb7fe9a9d78c34ca3d1627854e60b38
Danny Wong <DannyWang0619@gmail.com>
2020-03-26 11:10:48 -0600

    LtcM0377

--- LtcM0377CombinationSum.js
+++ LtcM0377CombinationSum.js
@@ -1,3 +1,17 @@
 var combinationSum4 = function(nums, target) {
-
+    let dp = Array(target+1);
+    dp[0] = 1;
+    for (let i=1; i<dp.length; i++)
+        dp[i] = 0;
+    
+    for (let i=1; i<target+1; i++){
+        for (let j=0; j<nums.length; j++){
+            num = nums[j];
+            if (i>=num) dp[i]+=dp[i-num];
+        }
+    }
+    return dp[target];
+    //return dp;
 }
+
+console.log(combinationSum4([1,2,3], 4));
