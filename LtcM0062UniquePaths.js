bd6972a41b8f2211476475942b522282fa4526ea
Danny Wong <DannyWang0619@gmail.com>
2020-03-27 09:49:16 -0600

    another solve UniquePaths

--- LtcM0062UniquePaths.js
+++ LtcM0062UniquePaths.js
@@ -1,4 +1,21 @@
-var uniquePaths = function(m, n) {
+var uniquePaths = function(m, n){
+    var dp = Array(n);
+    for (let i=0; i<n; i++){
+        dp[i] = Array(m);
+        for (let j=0; j<m; j++){
+            if ( (i===0)||(j===0) ) dp[i][j] = 1;
+        }
+    }
+
+    for (let i=1; i<n; i++)
+        for (let j=1; j<m; j++)
+            dp[i][j] = dp[i-1][j] + dp[i][j-1];
+
+    return dp[n-1][m-1];        
+    //console.log(dp);
+}
+
+/*var uniquePaths = function(m, n) {
     let rt = 0;
 
     dp = Array(n.length);
@@ -15,6 +32,6 @@ var uniquePaths = function(m, n) {
         if ( (m===1)||(n===1) ) return 0;
         return 1 + pathsHelp(m-1, n) + pathsHelp(m, n-1);
     }
-};
+};*/
 
 console.log(uniquePaths(7,3));
