5e99ba25fba13bf195713cc4893b5a4d199933ca
Danny Wong <DannyWang0619@gmail.com>
2020-03-28 09:45:43 -0600

    LtcM0139

--- LtcM0139WordBreak.js
+++ LtcM0139WordBreak.js
@@ -1,17 +1,27 @@
 var wordBreak = function(s, wordDict) {
     if (s.length===0) return true;
 
-    for (let i=1; i<s.length+1; i++)
+    for (let i=1; i<s.length+1; i++) {
         substr = s.slice(0,i);
         if ( (wordDict[substr]) && (wordBreak(s.slice(i, s.length+1), wordDict)) )
             return true;
-    
+    }
+
     return false;
 }
 
-wD = { 
+wordDict = { 
         "leet":true,
-        "code":true
+        "code":true,
+        "apple":true,
+        "pen":true,
+        "cats":true,
+        "dog":true,
+        "sand":true,
+        "and":true,
+        "cat":true
      };
 
-console.log(wordBreak("leetcode", wD));
+console.log(wordBreak("leetcode", wordDict));
+console.log(wordBreak("applepenapple", wordDict));
+console.log(wordBreak("catsandog", wordDict));
